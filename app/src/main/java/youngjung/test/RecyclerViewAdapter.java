d4acff4e08f6c2b73f8262ea8f0867e60cf638b2
JY <pyeonjy97@gmail.com>
2018-04-07 17:35:41 +0900

    170407_commit

--- app/src/main/java/youngjung/test/RecyclerViewAdapter.java
+++ app/src/main/java/youngjung/test/RecyclerViewAdapter.java
@@ -1,10 +1,12 @@
 package youngjung.test;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -15,14 +17,16 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     private String[] mData = new String[0];
     private String[] mName = new String[0];
     private String[] mPrice = new String[0];
+    private Boolean[] mStamp = new Boolean[0];
     private LayoutInflater mInflater;
     private ItemClickListener mClickListener;
 
-    RecyclerViewAdapter(Context context, String[] data, String[] name, String[] price) {
+    RecyclerViewAdapter(Context context, String[] data, String[] name, String[] price, Boolean[] stamp) {
         this.mInflater = LayoutInflater.from(context);
         this.mData = data;
         this.mName = name;
         this.mPrice = price;
+        this.mStamp = stamp;
     }
 
     @Override
@@ -36,26 +40,37 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         String animal = mData[position];
         String test = mName[position];
         String price = mPrice[position];
+        Boolean stamp = mStamp[position];
+
         holder.receiptDate.setText(animal);
         holder.receiptName.setText(test);
         holder.receiptPrice.setText(price);
+        if (stamp) {
+            // 허
+            holder.receiptStamp.setImageResource(R.drawable.ok_stamp);
+        } else {
+            // 불허
+            holder.receiptStamp.setImageResource(R.drawable.no_stamp);
+        }
     }
 
     @Override
     public int getItemCount() {
-        return mData.length;
+        return mName.length;
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView receiptName;
         TextView receiptDate;
         TextView receiptPrice;
+        ImageView receiptStamp;
 
         public ViewHolder(View itemView) {
             super(itemView);
             receiptName = itemView.findViewById(R.id.receipt_name);
             receiptDate = itemView.findViewById(R.id.receipt_date);
             receiptPrice = itemView.findViewById(R.id.receipt_price);
+            receiptStamp = itemView.findViewById(R.id.receipt_list_stamp);
             itemView.setOnClickListener(this);
         }
 
@@ -66,7 +81,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     String getItem(int id) {
-        return mData[id];
+        return mName[id];
     }
 
     void setClickListener(ItemClickListener itemClickListener) {
