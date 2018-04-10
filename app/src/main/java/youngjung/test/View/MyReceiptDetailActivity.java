353eaecdc2d0de7ecb0de773869f0773df6937c7
JY <pyeonjy97@gmail.com>
2018-04-10 19:04:19 +0900

    디테일페이지 레이아웃 완성

--- app/src/main/java/youngjung/test/View/MyReceiptDetailActivity.java
+++ app/src/main/java/youngjung/test/View/MyReceiptDetailActivity.java
@@ -2,6 +2,7 @@ package youngjung.test.View;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 import youngjung.test.R;
 import youngjung.test.ui.base.baseActivity;
@@ -10,7 +11,7 @@ import youngjung.test.ui.base.baseActivity;
  * Created by jy on 09/04/2018.
  */
 
-public class MyReceiptDetailActivity extends baseActivity {
+public class MyReceiptDetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
