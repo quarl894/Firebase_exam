56fa610735b09b075d896c9575432d53557432f5
quarl894 <quarl894@gmail.com>
2018-05-13 16:52:35 +0900

    에러확인

--- app/src/main/java/youngjung/test/Fragment/MainFragment.java
+++ app/src/main/java/youngjung/test/Fragment/MainFragment.java
@@ -72,7 +72,9 @@ public class MainFragment extends Fragment{
         tv_goal = rootView.findViewById(R.id.tv_goal);
         tv_goal_money = rootView.findViewById(R.id.tv_goal_money);
         tv_acc_money = rootView.findViewById(R.id.tv_acc_money);
-        tv_acc_money.setText(app.Moneyfomat(Integer.parseInt(dbHelper.get_money())));
+        tv_acc_money.setText(dbHelper.get_money());
+
+        Log.e("acc_money: ","" +dbHelper.get_money());
 
         databaseReference = FirebaseDatabase.getInstance().getReference();
         final String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
@@ -168,6 +170,5 @@ public class MainFragment extends Fragment{
     @Override
     public void onResume() {
         super.onResume();
-        tv_acc_money.setText(app.Moneyfomat(Integer.parseInt(dbHelper.get_money())));
     }
 }
