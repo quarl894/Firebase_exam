ec0098284bc78a3d750657add69ac25d26b82bb2
테스팅중 <Park_Soo_Young@bagsuyeong-ui-MacBook-Pro.local>
2018-05-13 18:47:20 +0900

    change

---
+++ app/src/main/java/youngjung/test/info/Info1.java
@@ -0,0 +1,13 @@
+package youngjung.test.info;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import youngjung.test.R;
+
+public class Info1 extends AppCompatActivity{
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info1);
+    }
+}
